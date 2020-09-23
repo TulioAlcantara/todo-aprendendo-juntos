d2840b351c1b02e75acc0a0575db48571ccdbdad
Bruno Rocha de Sousa <brunorsousa1998@gmail.com>
2020-09-23 02:13:21 -0300

    Adicionado font awesome

--- index.js
+++ index.js
@@ -2,37 +2,21 @@ const addButton = document.querySelector("#btn-add-item");
 const todoList = document.querySelector("#todo-list");
 const input = document.querySelector("#input-add-item");
 
-const calcButton = document.querySelector("#btn-number-check");
-const numberOutput = document.querySelector("#number-output");
-const inputNumber = document.querySelector("#input-number-check");
-
 window.onload = function () {
     addButton.addEventListener("click", addButtonFunc)
-
-    calcButton.addEventListener("click", calcButtonFunc)
 }
 
 let addButtonFunc = function (e) {
     let inputText = input.value;
-    const li = document.createElement("li");
+    let li = document.createElement("li");
+    let i = document.createElement("i");
 
+    i.className += "fas fa-trash"
     li.appendChild(document.createTextNode(inputText));
     li.addEventListener("click", function (e) {
         todoList.removeChild(e.target);
     })
     todoList.appendChild(li);
+    todoList.appendChild(i);
     input.value = "";
-}
-
-let calcButtonFunc = function (e) {
-    let number = inputNumber.value;
-
-    if (number % 2 == 0) {
-        numberOutput.textContent = "É par";
-        numberOutput.style.color = "green";
-    }
-    else {
-        numberOutput.textContent = "É impar";
-        numberOutput.style.color = "red";
-    }
 }
